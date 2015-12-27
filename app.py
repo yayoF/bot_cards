f34745c81904227ed05f7808a13cefb5124f1ff5
cperezortiz <cperezortiz.90@gmail.com>
2015-12-27 12:05:17 -0500

    Send image from local path

--- app.py
+++ app.py
@@ -7,14 +7,19 @@ token = '135332404:AAH69WBxJHKDlrrN3cxiG9f5_VUtbqpHGOk'
 url = 'https://api.telegram.org/bot%s/' % token
 
 while True:
+	# We request for new messages in the telegram server
 	get_updates = json.loads(requests.get(url + 'getUpdates', params=dict(offset=last_update+1)).text)
 	for update in get_updates['result']:
 		if last_update < update['update_id']:
 			last_update = update['update_id']
 			if update.get('message') != None:
-				# print("mensaje: " + str(update['update_id'])
+				# Here we handle the new messages content
 				if update['message'].get('text') != None:
-					# es un mensaje con texto, enviemos una respeusta
-					requests.get(url + 'sendMessage', params=dict(chat_id=update['message']['chat']['id'], text=update['message']['text']))
-					# print("texto: " + update['message']['text'])
+					if update['message']['text'] == 'crazyV':
+						data = {'chat_id': update['message']['chat']['id']}
+						files = {'photo': ('/Users/christianperez/Dropbox/bot_images/the_hound.jpg', open('/Users/christianperez/Dropbox/bot_images/the_hound.jpg', 'rb'))}
+						requests.post(url + 'sendPhoto', data=data, files=files)
+					else:
+						requests.get(url + 'sendMessage', params=dict(chat_id=update['message']['chat']['id'], text=update['message']['text']))
+						# print("texto: " + update['message']['text'])
 	sleep(3)
